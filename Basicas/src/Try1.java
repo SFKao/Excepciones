e6592f4839e0a2af709fe68d228d56df57a4e70c
Oscar <omelcam934@g.educaand.es>
2022-02-08 18:55:40 +0100

    Ej1 y profesor

---
+++ Basicas/src/Try1.java
@@ -0,0 +1,6 @@
+public class Try1 {
+    public static void main(String[] arg) {
+        int[] array = new int[20];
+        array[-3] = 24;
+    }
+}
---
+++ ExplicacionesProfe/src/DivisionPorCero.java
@@ -0,0 +1,49 @@
+public class DivisionPorCero {
+    public static void main(String[] args) {
+        System.out.println(divisionEnteroSinExcepciones(2,0));
+        System.out.println(divisionEnteroSinExcepciones(3,3));
+
+        try {
+            System.out.println(divisionEnteroConExcepciones(3,0));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        divisionEnterosConExcepcionesSinThrow(2,0);
+        divisionEnterosConExcepcionesSinThrow(2,2);
+    }
+
+    public static int divisionEnteroSinExcepciones(int dividendo, int divisor){
+        int rest = 0;
+
+        if(divisor!=0){
+            rest = dividendo/divisor;
+        }else{
+            System.out.println("Error, division por 0");
+        }
+
+        return rest;
+    }
+
+    public static int divisionEnteroConExcepciones(int dividendo, int divisor) throws Exception{
+        int rest;
+        if(divisor!=0){
+            rest = dividendo/divisor;
+        }else{
+            throw new Exception("Dividiendo entre 0!!");
+        }
+        return rest;
+    }
+
+    public static void divisionEnterosConExcepcionesSinThrow(int dividendo, int divisor){
+        int res = 0;
+        try{
+            res = dividendo/divisor;
+        }catch(ArithmeticException a){
+            System.out.println("Division por 0");
+            System.out.println(a);
+        }finally {
+            System.out.println("Res = " + res);
+        }
+    }
+}
